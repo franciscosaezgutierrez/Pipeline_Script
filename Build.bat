7119784188d9413b0aa0b337071fa56ca7730735
Francisco Sáez Gutiérrez <38472010+franciscosaezgutierrez@users.noreply.github.com>
2019-11-07 13:05:03 +0100

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 echo "Building the Project : %date% : %time%"
-git clone git://github.com/franciscosaezgutierrez/Spring3MVC.git mygrit
 cd C:\apache-maven-3.6.0\bin
+git clone git://github.com/franciscosaezgutierrez/Spring3MVC.git mygrit
 mvn clean package
