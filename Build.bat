cdd09a0d11c12d0c0e54c824ed9372ca4067ed9c
Francisco Sáez Gutiérrez <38472010+franciscosaezgutierrez@users.noreply.github.com>
2019-11-07 13:23:04 +0100

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,4 +1,6 @@
 echo "Building the Project : %date% : %time%"
+SET JAVA_HOME="C:\Program Files\Java\jdk1.8.0_77"
+SET JAVA_JRE="C:\Program Files\Java\jre1.8.0_77"
 git clone git://github.com/franciscosaezgutierrez/Spring3MVC.git C:\SpringMVC
 cd C:\SpringMVC
 C:\apache-maven-3.6.0\bin\mvn package
