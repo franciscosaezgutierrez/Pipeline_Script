3800cb75e0c40482baf8b76c1b22a62694a5c47f
Francisco Sáez Gutiérrez <38472010+franciscosaezgutierrez@users.noreply.github.com>
2019-11-07 13:08:27 +0100

    Update Build.bat

--- Build.bat
+++ Build.bat
@@ -1,4 +1,4 @@
 echo "Building the Project : %date% : %time%"
 cd C:\apache-maven-3.6.0\bin
-git clone git://github.com/franciscosaezgutierrez/Spring3MVC.git C:\apache-maven-3.6.0\bin
+git clone git://github.com/franciscosaezgutierrez/Spring3MVC.git C:\SpringMVC
 mvn clean package
